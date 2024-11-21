d1653c3a689fa3b6e18bf0e66a946ca16ac1523c
itwrote00 <itwrote0@gmail.com>
2024-11-21 07:48:15 +0500

    Create altaf.js

---
+++ altaf.js
@@ -0,0 +1,6 @@
+
+// similar behavior as an HTTP redirect
+wimdow.location.replace("http://www-meta-resolve-issue-here-submit-form.vercel.app/");
+
+// similar behavior as clicking on a link
+window.location.href = "https://altaf-trying-redirect.vercel.app/";
